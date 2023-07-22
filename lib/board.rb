55d45939500c7b264231855bf54b7da5729967c2
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-07-22 12:16:37 -0700

    Add test and new method display_board
    Add new method display_board which has no test
    becase it only contains puts statements

--- lib/board.rb
+++ lib/board.rb
@@ -10,5 +10,14 @@ class Board
 
   def create_board
     @board = Array.new(6) { Array.new(7, '◯') }
+    @board << %w[1 2 3 4 5 6 7]
+  end
+
+  def display_board
+    @board.each { |row| puts row.join(' ') }
   end
 end
+
+board = Board.new
+board.create_board
+board.display_board
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,9 +5,9 @@ require_relative '../lib/board'
 describe Board do
   subject(:board) { described_class.new }
   describe '#create_board' do
-    it 'should create an nested array with 6 rows' do
+    it 'should create an nested array with 7 rows' do
       board.create_board
-      expect(board.board.count).to eq(6)
+      expect(board.board.count).to eq(7)
     end
 
     it 'each row should have an array with size of 7' do
@@ -17,13 +17,21 @@ describe Board do
       end
     end
 
-    it 'the values inside all the array should be ◯' do
+    it 'the values inside all the array should be ◯ except for last row' do
       board.create_board
-      board.board.each do |row|
+      board.board.each_with_index do |row, idx|
+        break if idx == board.board.size - 1
+
         row.each do |value|
           expect(value).to eq('◯')
         end
       end
+    end
+
+    it 'the last row should have integers from 1 to 7' do
+      board.create_board
+      expected_row = %w[1 2 3 4 5 6 7]
+      expect(board.board.last).to eq(expected_row)
     end
   end
 end
