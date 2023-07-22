d896b851b0cc954724867cbc86d44824dfb675df
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-07-22 15:20:34 -0700

    add new test for #check_column and #change_board
    check_column will check the columns of the board
    and see if theres already something there.
    change board will take in player input and change
    the board.

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -65,15 +65,15 @@ describe ConnectFour do
     subject(:game_turn) { described_class.new }
     context 'everytime this method is called, the players switch turn' do
       it 'changes turn from player 1 to player 2' do
-        last_turn = 'player 1'
+        last_turn = '1'
         current_turn = game_turn.change_turn(last_turn)
-        expect(current_turn).to be('player 2')
+        expect(current_turn).to be('2')
       end
 
       it 'changes turn from player 2 to palyer 1' do
-        last_turn = 'player 2'
+        last_turn = '2'
         current_turn = game_turn.change_turn(last_turn)
-        expect(current_turn).to be('player 1')
+        expect(current_turn).to be('1')
       end
     end
   end
@@ -83,13 +83,55 @@ describe ConnectFour do
     context 'when called, returns who\'s turn it is' do
       it 'returns player 1' do
         current_turn = game_turn.whos_turn
-        expect(current_turn).to be('player 1')
+        expect(current_turn).to be('1')
       end
 
       it 'returns player 2' do
         game_turn.change_turn('player 1')
         current_turn = game_turn.whos_turn
-        expect(current_turn).to be('player 2')
+        expect(current_turn).to be('2')
+      end
+    end
+  end
+
+  describe '#check_column' do
+    subject(:game_column) { described_class.new }
+    context 'check the availability of a given column' do
+      player_input = 3
+      game_column.board.each_with_index do |row, idx|
+        row[player_input] = '1' if idx > 2 && idx < 5
+      end
+      results = game_column.check_column(player_input)
+      it 'returns which row is good' do
+        expect(results).to be(2)
+      end
+
+      game_column.board.each_with_index do |row, idx|
+        row[player_input] = '1' if idx < 5
+      end
+      results = game_column.check_column(player_input)
+      it 'returns nil' do
+        expect(results).to be(nil)
+      end
+    end
+  end
+
+  describe '#change_board' do
+    subject(:game) { described_class.new }
+    context 'when player input is 3' do
+      player_input = 3
+      it 'will change board in row 5, col 2 to 1' do
+        allow(game).to receive(:whos_turn).and_return('1')
+        allow(game).to receive(:check_column).and_return(5)
+        game.change_board(player_input)
+        expect(game.board[5][player_input]).to be('1')
+      end
+
+      it 'will change board in row 5, col 2 to 2' do
+        allow(game).to receive(:whos_turn).and_return('2')
+        allow(game).to receive(:check_column).and_return(5)
+        game.change_board(player_input)
+        expect(game.board[5][player_input]).to be('2')
       end
     end
   end
